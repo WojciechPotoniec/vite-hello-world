0fdc4641408e16cb598196bb86f96319fe23f523
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-09 17:15:52 +0200

    created component with img

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img class="img-fluid" src="/public/img/jumbo.png" alt="Hero-image">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppHero",
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
