ea04698413823cda29c2fd5d72abedf870b5ea60
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-09 17:16:50 +0200

    created main component with imported AppTitle and Apphero

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <AppTitle />
+    <AppHero />
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+export default {
+    components: {
+        AppTitle,
+        AppHero
+    },
+    data() {
+      return {
+        
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
