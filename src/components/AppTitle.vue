9752722af6ff9f470c8fba9e43553f66bd4b2904
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-09 17:15:29 +0200

    created component with title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <h1>{{ Apptitle }}</h1>
+    </main>
+</template>
+
+<script>
+
+    export default {
+        name: "AppTitle",
+        data() {
+            return {
+                Apptitle: 'La mia prima app con Vite!'
+            }
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
